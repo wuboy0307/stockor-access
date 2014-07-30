ba9bf7c3b7fb6cc84a09871daa1ad9cd1e580804
Nathan Stitt <nathan@stitt.org>
2014-07-29 21:22:00 -0500

    Remove commented out dead code

--- lib/skr/access/roles/accounting.rb
+++ lib/skr/access/roles/accounting.rb
@@ -9,21 +9,8 @@ module Skr
 
                 lock_writes Customer, :terms_id
             end
+
         end
-
-        #LockedFields.lock Customer, :terms_id, to: Accounting, only: :write
-
-        # module Customer
-
-        #     def json_attribute_is_allowed?( name, user )
-        #         if name == :gl_receivables_account_id
-        #             user.roles
-        #         else
-        #             super
-        #         end
-        #     end
-        # end
-        # Skr::Customer.send :extend, Customer
 
     end
 end
