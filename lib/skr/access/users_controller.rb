f37fe1137980b8f4bcdcda3c570866822dd71b81
Nathan Stitt <nathan@stitt.org>
2014-09-21 18:18:04 -0500

    Add endpoint for logging out via deleting session

--- lib/skr/access/users_controller.rb
+++ lib/skr/access/users_controller.rb
@@ -1,18 +1,19 @@
 module Skr
 
-    module Access
-
-        Skr::API::Root.post "login" do
-            user = User.find_by(login: params.data.login)
-            if user && user.authenticate(params.data.password)
-                { success: true, message: "Login succeeded", data: user.workspace_data }
-            else
-                { success: false, message: "Login failed", data: {} }
-            end
+    API::Root.post "login" do
+        user = User.find_by(login: params.data.login)
+        if user && user.authenticate(params.data.password)
+            { success: true, message: "Login succeeded", data: user.workspace_data }
+        else
+            { success: false, message: "Login failed", data: {} }
         end
-
     end
 
+    API::Root.delete "login/:id" do
+        session.destroy
+        { success: true, message: "Logout succeeded", data: {} }
+    end
 
-    Skr::API::Root.build_route User
+    API::Root.build_route User
+
 end
