548c4532e795a08837e9cab32d4ae66a811ccf22
Nathan Stitt <nathan@stitt.org>
2014-09-21 18:06:01 -0500

    Move function to Access

--- lib/skr/access.rb
+++ lib/skr/access.rb
@@ -5,6 +5,37 @@ module Skr
   module Access
       # add our migrations to the generator
       Skr::Core::DB::Migrations.paths << Pathname.new(__FILE__).dirname.join("../../db/migrate").realpath
+
+      class << self
+
+          def _type_to_str(klass)
+              klass.to_s.demodulize.underscore
+          end
+
+          def for_user( user )
+              {
+                  :roles => user.roles.map{ | role |
+                      {
+                          type: _type_to_str(role.class),
+                          read: role.read.map{ |klass| _type_to_str(klass) },
+                          write: role.write.map{ |klass| _type_to_str(klass) },
+                          delete: role.delete.map{ |klass| _type_to_str(klass) }
+                      }
+                  },
+                  :locked_fields => LockedFields.definitions.map{ | klass, locks |
+                      {
+                          type: _type_to_str(klass),
+                          locks: locks.each_with_object({}) do |(field, grants), result|
+                              result[field] = grants.map do |grant|
+                                  { role: _type_to_str(grant[:role]), only: grant[:only] }
+                              end
+                          end
+                      }
+                  }
+              }
+          end
+
+      end
   end
 end
 
@@ -13,7 +44,6 @@ require_relative 'access/role'
 require_relative 'access/role_collection'
 require_relative 'access/global_grants'
 
-require_relative 'access/serializer'
 require_relative 'access/api_authentication_provider'
 require_relative 'user'
 require_relative 'access/users_controller'
